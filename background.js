6f758ee5e42c9577dc2d546ce32bf8df5d20572d
capprojectuse <srajasek@stedwards.edu>
2025-03-12 19:07:49 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,11 +1,3 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.contextMenus.create({
-    id: "factCheck",
-    title: "Fact-check: '%s'",
-    contexts: ["selection"]
-  });
-});
-
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === "factCheck") {
     let selectedText = info.selectionText;
@@ -15,10 +7,23 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
       headers: {
         "Content-Type": "application/json"
       },
-      body: JSON.stringify({ text: selectedText })
+      body: JSON.stringify({ claim: selectedText })
     })
-    .then(response => response.json())
-    .then(data => {
+    .then(response => {
+      console.log("Raw response:", response);
+      return response.text();
+    })
+    .then(text => {
+      console.log("Response text:", text);
+
+      let data;
+      try {
+        data = JSON.parse(text);  
+      } catch (error) {
+        console.error("Error: API did not return valid JSON. Response was:", text);
+        return;
+      }
+
       let resultPageURL = `http://192.241.155.25/result?data=${encodeURIComponent(JSON.stringify(data))}`;
       chrome.tabs.create({ url: resultPageURL });
     })
